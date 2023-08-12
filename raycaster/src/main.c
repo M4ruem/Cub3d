5d33f6ede4445c796dd544c6d38ec0f4c249e72b
Camille Ormiere <cormiere@2C5.42angouleme.fr>
2023-08-12 11:13:44 +0200

    adding mlx and first drawing function

---
+++ raycaster/src/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <MLX42/MLX42.h>
+
+#define WIDTH 512
+#define HEIGHT 512
+
+static mlx_image_t*	image;
+
+void ft_set_color(void* arg)
+{
+	(void)arg;
+	int x;
+	int y;
+
+	x = 0;
+	while (x < (int)image->width)
+	{
+		y = 0;
+		while (y < (int)image->height)
+		{
+			mlx_put_pixel(image, x, y, 0xFFFF00FF);
+			y++;
+		}
+		x++;
+	}
+}
+
+void ft_hook(void* arg)
+{
+	mlx_t* mlx = arg;
+
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		image->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		image->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		image->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		image->instances[0].x += 5;
+}
+
+int main(int ac, const char** av)
+{
+	(void)ac;
+	(void)av;
+
+	mlx_t* mlx;
+
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", false)))
+	{
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	draw_map2D();
+	if (!(image = mlx_new_image(mlx, 10, 10)))
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
+	{
+		mlx_close_window(mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+
+	mlx_loop_hook(mlx, ft_set_color, mlx);
+	mlx_loop_hook(mlx, ft_hook, mlx);
+
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
